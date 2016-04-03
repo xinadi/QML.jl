b84a2791af6564640cb0c164a28264e9d05e2fc7
Bart Janssens <bart@bartjanssens.org>
2016-04-04 00:35:28 +0200

    Add qmlget macro

--- src/QML.jl
+++ src/QML.jl
@@ -1,7 +1,28 @@
+module QML
+
 @windows_only ENV["QML_PREFIX_PATH"] = joinpath(Pkg.dir("QML"),"deps","usr")
 using CxxWrap
-wrap_modules(CxxWrap.lib_path(joinpath(Pkg.dir("QML"),"deps","usr","lib","libqmlwrap")))
+wrap_module(CxxWrap.lib_path(joinpath(Pkg.dir("QML"),"deps","usr","lib","libqmlwrap")))
 
+generic_property_get(ctx::QQmlContext, key::AbstractString) = context_property(ctx, key)
+generic_property_get(o::JuliaObject, key::AbstractString) = value(o, key)
+
+macro expand_dots(source_expr, func)
+  if isa(source_expr, Expr) && source_expr.head == :(.)
+    return :($func(@expand_dots($(esc(source_expr.args[1])), $func), $(string(source_expr.args[2].args[1]))))
+  end
+  return esc(source_expr)
+end
+
+macro qmlget(dots_expr)
+  :(@expand_dots($(esc(dots_expr)), generic_property_get))
+end
+
+macro qmlset(assign_expr)
+  :(generic_property_set(@expand_dots($(assign_expr.args[1].args[1]), generic_property_get), $(string(assign_expr.args[1].args[2].args[1])), $(assign_expr.args[2])))
+end
+
+export @qmlget
 
 @doc """
 Module for building [Qt5 QML](http://doc.qt.io/qt-5/qtqml-index.html) graphical user interfaces for Julia programs.
@@ -13,14 +34,14 @@ the [Qt documentation](http://doc.qt.io/qt-5/qtqml-index.html) for details inste
 Create a new [QApplication](http://doc.qt.io/qt-5/qapplication.html). This function generates
 a writable `argc` and `argv` as required by Qt. The returned object must be manually finalized
 before julia exit.
-""" QML.application
+""" application
 
-@doc "Equivalent to [`QApplication::exec`](http://doc.qt.io/qt-5/qapplication.html#exec)" QML.exec
+@doc "Equivalent to [`QApplication::exec`](http://doc.qt.io/qt-5/qapplication.html#exec)" exec
 
 @doc """
 Equivalent to [`QQmlContext::setContextProperty`](http://doc.qt.io/qt-5/qqmlcontext.html#setContextProperty).
 
-This function is useful to expose Julia values to QML. Current properties can be fundamental numeric types,
+This function is useful to expose Julia values to  Current properties can be fundamental numeric types,
 strings and any `QObject`.
 
 Example:
@@ -31,27 +52,29 @@ set_context_property(root_ctx, "my_property", 1)
 ```
 
 You can now use `my_property` in QML and every time `set_context_property` is called on it the GUI gets notified.
-""" QML.set_context_property
+""" set_context_property
 
-@doc "Equivalent to [`QQmlEngine::rootContext`](http://doc.qt.io/qt-5/qqmlengine.html#rootContext)" QML.root_context
+@doc "Equivalent to [`QQmlEngine::rootContext`](http://doc.qt.io/qt-5/qqmlengine.html#rootContext)" root_context
 
 @doc """
 Equivalent to [`&QQmlApplicationEngine::load`](http://doc.qt.io/qt-5/qqmlapplicationengine.html#load-1). The first argument is
 the application engine, the second is a string containing a path to the local QML file to load.
-""" QML.load
+""" load
 
-@doc "Equivalent to `QLibraryInfo::location(QLibraryInfo::PrefixPath)`" QML.qt_prefix_path
-@doc "Equivalent to `QQuickWindow::contentItem`" QML.content_item
-@doc "Equivalent to `QQuickView::setSource`" QML.set_source
-@doc "Equivalent to `QQuickView::show`" QML.show
-@doc "Equivalent to `QQuickView::engine`" QML.engine
-@doc "Equivalent to `QQuickView::rootObject`" QML.root_object
+@doc "Equivalent to `QLibraryInfo::location(QLibraryInfo::PrefixPath)`" qt_prefix_path
+@doc "Equivalent to `QQuickWindow::contentItem`" content_item
+@doc "Equivalent to `QQuickView::setSource`" set_source
+@doc "Equivalent to `QQuickView::show`" show
+@doc "Equivalent to `QQuickView::engine`" engine
+@doc "Equivalent to `QQuickView::rootObject`" root_object
 
 @doc """
 Equivalent to `QQmlComponent::setData`. Use this to set the QML code for a QQmlComponent from a Julia string literal.
-""" QML.set_data
+""" set_data
 
 @doc """
 Equivalent to `QQmlComponent::create`. This creates a component defined by the QML code set using `set_data`.
 It also makes sure the newly created object is parented to the given context.
-""" QML.create
+""" create
+
+end
