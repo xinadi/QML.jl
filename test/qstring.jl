26d113df4634eec6a32f8281c4976d47e5a22773
Bart Janssens <bart@bartjanssens.org>
2019-08-21 07:54:18 +0200

    Add QStringList

--- test/qstring.jl
+++ test/qstring.jl
@@ -2,11 +2,17 @@ using CxxWrap
 using QML
 using Test
 
-for s in ["TestStr", "😁😃😆abc😎😈☹"]
-    qs = QString(s)
-    @test qs == s
-    io = IOBuffer()
-    write(io, qs)
-    @test String(take!(io)) == s
-    @test length(qs) == length(s)
+let strings = ["TestStr", "😁😃😆abc😎😈☹"]
+  qsl = QStringList()
+  for s in strings
+      qs = QString(s)
+      @test qs == s
+      io = IOBuffer()
+      write(io, qs)
+      @test String(take!(io)) == s
+      @test length(qs) == length(s)
+      push!(qsl, s)
+  end
+
+  @test strings == qsl
 end
