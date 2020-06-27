1e414db83e1a6e6fd2689220838e5059c93e56c3
Bart Janssens <bart@bartjanssens.org>
2020-06-27 15:02:41 +0200

    Proper support for Makie rendering

--- src/makie_support.jl
+++ src/makie_support.jl
@@ -4,45 +4,60 @@ using ..Makie
 using ..Makie.GLMakie.Colors
 using ..Makie.GLMakie.FixedPointNumbers
 using ..Makie.GLMakie.ModernGL
+using ..Makie.GLMakie.StaticArrays
+using ..Makie.GLMakie.LinearAlgebra
 using QML
 using CxxWrap
+
+const GLMakie = Makie.GLMakie
+const GLAbstraction = GLMakie.GLAbstraction
+using .GLAbstraction
+
 
 mutable struct QMLGLContext
   valid::Bool
   fbo::CxxPtr{QML.QOpenGLFramebufferObject}
 end
 
-Makie.GLMakie.ShaderAbstractions.native_switch_context!(ctx::QMLGLContext) = QML.bind(ctx.fbo)
+GLMakie.ShaderAbstractions.native_switch_context!(ctx::QMLGLContext) = QML.bind(ctx.fbo)
 
-function Makie.GLMakie.ShaderAbstractions.native_context_alive(ctx::QMLGLContext)
+function GLMakie.ShaderAbstractions.native_context_alive(ctx::QMLGLContext)
   return ctx.valid
 end
 
-const ScreenID = Makie.GLMakie.ScreenID
-const ZIndex = Makie.GLMakie.ZIndex
-const RenderObject = Makie.GLMakie.RenderObject
-const ScreenArea = Makie.GLMakie.ScreenArea
+const ScreenID = GLMakie.ScreenID
+const ZIndex = GLMakie.ZIndex
+const RenderObject = GLMakie.RenderObject
+const ScreenArea = GLMakie.ScreenArea
 
-mutable struct QMLScreen <: Makie.GLMakie.GLScreen
-  size::Tuple{Int,Int}
+@cxxdereference function sizetuple(fbo::QML.QOpenGLFramebufferObject)
+  fbosize = QML.size(fbo)
+  return (Int(QML.width(fbosize)), Int(QML.height(fbosize)))
+end
+
+mutable struct QMLScreen <: GLMakie.GLScreen
   screen2scene::Dict{WeakRef, ScreenID}
   screens::Vector{ScreenArea}
   renderlist::Vector{Tuple{ZIndex, ScreenID, RenderObject}}
   cache::Dict{UInt64, RenderObject}
   cache2plot::Dict{UInt16, AbstractPlot}
   framecache::Tuple{Matrix{RGB{N0f8}}, Matrix{RGB{N0f8}}}
+  framebuffer::GLMakie.GLFramebuffer
+  qmlfbo::QML.QOpenGLFramebufferObject
 
-  function QMLScreen(w, h)
+  @cxxdereference function QMLScreen(fbo::QML.QOpenGLFramebufferObject)
+    fbosize = sizetuple(fbo)
     newscreen = new(
-      (w,h),
       Dict{WeakRef, ScreenID}(),
       ScreenArea[],
       Tuple{ZIndex, ScreenID, RenderObject}[],
       Dict{UInt64, RenderObject}(),
-      Dict{UInt16, AbstractPlot}()
+      Dict{UInt16, AbstractPlot}(),
+      (Matrix{RGB{N0f8}}(undef, fbosize), Matrix{RGB{N0f8}}(undef, reverse(fbosize))),
+      GLMakie.GLFramebuffer(fbosize),
+      fbo
     )
     finalizer(newscreen) do s
-      # save_print("Freeing screen")
       empty!.((s.renderlist, s.screens, s.cache, s.screen2scene, s.cache2plot))
       return
     end
@@ -50,37 +65,130 @@ mutable struct QMLScreen <: Makie.GLMakie.GLScreen
 end
 
 Base.isopen(screen::QMLScreen) = true
-Makie.GLMakie.GeometryBasics.widths(screen::QMLScreen) = screen.size
+GLMakie.GeometryBasics.widths(screen::QMLScreen) = sizetuple(screen.qmlfbo)
 
-function Makie.GLMakie.render_frame(screen::QMLScreen)
-  Makie.GLMakie.setup!(screen)
-  for (zindex, screenid, elem) in screen.renderlist
-    Makie.GLMakie.render(elem)
+# From rendering.jl in GLMakie, with only slight adaptations
+function GLMakie.render_frame(screen::QMLScreen; resize_buffers=true)
+  w, h = sizetuple(screen.qmlfbo)
+  fb = screen.framebuffer
+  if resize_buffers
+    resize!(fb, (w,h))
   end
+
+  # prepare stencil (for sub-scenes)
+  glEnable(GL_STENCIL_TEST)
+  glBindFramebuffer(GL_FRAMEBUFFER, fb.id[1]) # color framebuffer
+  glDrawBuffers(4, [
+    GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1,
+    GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3
+  ])
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0xff)
+  glClearStencil(0)
+  glClearColor(0,0,0,0)
+  glClear(GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT)
+  GLMakie.setup!(screen)
+
+  # render with FXAA & SSAO
+  glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  GLAbstraction.render(screen, true, true)
+
+  # SSAO - calculate occlusion
+  glDrawBuffer(GL_COLOR_ATTACHMENT4)  # occlusion buffer
+  glViewport(0, 0, w, h)
+  glClearColor(1, 1, 1, 1)            # 1 means no darkening
+  glClear(GL_COLOR_BUFFER_BIT)
+
+  for (screenid, scene) in screen.screens
+    # update uniforms
+    SSAO = scene.SSAO
+    uniforms = fb.postprocess[1].uniforms
+    uniforms[:projection][] = scene.camera.projection[]
+    uniforms[:bias][] = Float32(to_value(get(SSAO, :bias, 0.025)))
+    uniforms[:radius][] = Float32(to_value(get(SSAO, :radius, 0.5)))
+    # use stencil to select one scene
+    glStencilFunc(GL_EQUAL, screenid, 0xff)
+    GLAbstraction.render(fb.postprocess[1])
+  end
+
+  # SSAO - blur occlusion and apply to color
+  glDrawBuffer(GL_COLOR_ATTACHMENT0)  # color buffer
+  for (screenid, scene) in screen.screens
+    # update uniforms
+    SSAO = scene.attributes.SSAO
+    uniforms = fb.postprocess[2].uniforms
+    uniforms[:blur_range][] = Int32(to_value(get(SSAO, :blur, 2)))
+
+    # use stencil to select one scene
+    glStencilFunc(GL_EQUAL, screenid, 0xff)
+    GLAbstraction.render(fb.postprocess[2])
+  end
+  glDisable(GL_STENCIL_TEST)
+
+  # render with FXAA but no SSAO
+  glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  GLAbstraction.render(screen, true, false)
+  glDisable(GL_STENCIL_TEST)
+
+  # FXAA - calculate LUMA
+  glBindFramebuffer(GL_FRAMEBUFFER, fb.id[2])
+  glDrawBuffer(GL_COLOR_ATTACHMENT0)  # color_luma buffer
+  glViewport(0, 0, w, h)
+  # necessary with negative SSAO bias...
+  glClearColor(1, 1, 1, 1)
+  glClear(GL_COLOR_BUFFER_BIT)
+  GLAbstraction.render(fb.postprocess[3])
+
+  # FXAA - perform anti-aliasing
+  glBindFramebuffer(GL_FRAMEBUFFER, fb.id[1])
+  glDrawBuffer(GL_COLOR_ATTACHMENT0)  # color buffer
+  # glViewport(0, 0, w, h) # not necessary
+  GLAbstraction.render(fb.postprocess[4])
+
+  # no FXAA primary render
+  glDrawBuffers(2, [GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1])
+  glEnable(GL_STENCIL_TEST)
+  glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE)
+  glStencilMask(0x00)
+  GLAbstraction.render(screen, false)
+  glDisable(GL_STENCIL_TEST)
+
+  # transfer everything to the screen, which in QML is the QOpenGLFramebufferObject we are rendering to
+  QML.bind(screen.qmlfbo)
+  glViewport(0, 0, w, h)
+  glClear(GL_COLOR_BUFFER_BIT)
+  GLAbstraction.render(fb.postprocess[5]) # copy postprocess
+
+  return
 end
 
 function Base.display(screen::QMLScreen, scene::Scene)
-  scene.events.window_area[] = AbstractPlotting.IRect(0,0,screen.size...)
+  scene.events.window_area[] = AbstractPlotting.IRect(0,0,sizetuple(screen.qmlfbo)...)
   empty!(screen)
   insertplots!(screen, scene)
   AbstractPlotting.update!(scene)
-  Makie.GLMakie.render_frame(screen)
+  GLMakie.render_frame(screen)
   return
 end
 
 function setup_screen(fbo)
   try
-    old_ctx = Makie.GLMakie.GLAbstraction.current_context()
+    old_ctx = GLMakie.GLAbstraction.current_context()
     old_ctx.valid = false
   catch
   end
-  Makie.GLMakie.ShaderAbstractions.switch_context!(QMLGLContext(true, fbo))
-  fbosize = QML.size(fbo)
-  return QMLScreen(QML.width(fbosize), QML.height(fbosize))
+  GLMakie.ShaderAbstractions.switch_context!(QMLGLContext(true, fbo))
+  return QMLScreen(fbo)
 end
 
 function on_context_destroy()
-  Makie.GLMakie.ShaderAbstractions.switch_context!()
+  GLMakie.ShaderAbstractions.switch_context!()
   return
 end
 
