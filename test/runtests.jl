a417769e3f2974431fec9450a95a26d2c6c46a40
Bart Janssens <bart@bartjanssens.org>
2023-09-24 01:09:03 +0200

    Run examples in separate directories

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,26 +27,30 @@ withenv("JULIA_LOAD_PATH" => nothing, "JULIA_GR_PROVIDER" => "BinaryBuilder") do
       examplesdir = mkdir("QmlJuliaExamples")
       LibGit2.clone("https://github.com/barche/QmlJuliaExamples.git", examplesdir; branch="qt6")
       cd(examplesdir) do
-        allowmakie = true
-        if get(ENV, "CI", "false") == "true" && (Sys.isapple() || Sys.iswindows())
-          allowmakie = false
-          filtered = filter(l -> !contains(l, "Makie"), collect(eachline("Project.toml")))
-          open("Project.toml", "w") do output
-            for l in filtered
-              println(output, l)
+        for d in ["basic", "images", "opengl", "plots"]
+          cd(d) do 
+            allowmakie = true
+            if get(ENV, "CI", "false") == "true" && (Sys.isapple() || Sys.iswindows())
+              allowmakie = false
+              filtered = filter(l -> !contains(l, "Makie"), collect(eachline("Project.toml")))
+              open("Project.toml", "w") do output
+                for l in filtered
+                  println(output, l)
+                end
+              end
             end
+
+            qmlpath = replace(dirname(dirname(pathof(QML))), "\\" => "/")
+            cxxpath = replace(dirname(dirname(pathof(QML.CxxWrap))), "\\" => "/")
+            updatecommand = """
+              using Pkg
+              Pkg.develop([PackageSpec(path="$qmlpath"), PackageSpec(path="$cxxpath")])
+              Pkg.precompile()
+            """
+            run(`$(Base.julia_cmd()) --project -e "$updatecommand"`)
+            QML.runexamples(allowmakie)
           end
         end
-
-        qmlpath = replace(dirname(dirname(pathof(QML))), "\\" => "/")
-        cxxpath = replace(dirname(dirname(pathof(QML.CxxWrap))), "\\" => "/")
-        updatecommand = """
-          using Pkg
-          Pkg.develop([PackageSpec(path="$qmlpath"), PackageSpec(path="$cxxpath")])
-          Pkg.precompile()
-        """
-        run(`$(Base.julia_cmd()) --project -e "$updatecommand"`)
-        QML.runexamples(allowmakie)
       end
     end
     println(pwd())
